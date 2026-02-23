848a8646e57331fc06c50bd38fee4152736242c3
antonella <antonellatong@outlook.com>
2026-02-23 09:41:27 -0800

    changes

---
+++ 2026-02-23/CCC '24 J2 - Dusa And The Yobis.py
@@ -0,0 +1,6 @@
+d = int(input())
+r = int(input())
+while d > r:
+    d += r
+    r = int(input())
+print(d)
---
+++ 2026-02-23/file.py
@@ -0,0 +1,7 @@
+#loops in python
+#white loops
+a = int(input())
+while a % 2 == 0:
+    print("this number is")
+    print("even")
+    a = int(input())
